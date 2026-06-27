0b771cf325487af9aca860c5b947c189716ace0d
Santiago Osorio <lopezos0218@gmail.com>
2026-06-26 21:24:21 -0500

    fix: mapear assets publicos en configuracion de server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     // nitro/vite builds from this
-    server: { entry: "server" },
+    server: { 
+      entry: "server",
+      // Añadimos esto para mapear los assets públicos en producción
+      publicAssets: [
+        {
+          dir: "./.output/public"
+        }
+      ]
+    },
   },
 });
